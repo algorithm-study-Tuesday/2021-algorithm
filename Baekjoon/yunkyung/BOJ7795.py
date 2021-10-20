5c4b9c896e3d886c54a6d7b1d8ff2ee7fe5f5766
expresshihway <03wow22@gmail.com>
2021-10-20 15:57:00 +0900

    fix: 7795 이분탐색 윤경

--- Baekjoon/yunkyung/BOJ7795.py
+++ Baekjoon/yunkyung/BOJ7795.py
@@ -1,27 +1,32 @@
 import sys
 
 input = sys.stdin.readline
+
+def binary_search(target, data):
+  left = 0
+  right = len(data) - 1
+  res = -1
+
+  while left <= right:
+    mid = (left + right) // 2
+    if data[mid] < target:
+      res = mid
+      left = mid + 1
+    else:
+      right = mid - 1
+  
+  return res
+
 t = int(input())
-result = []
 for i in range(t):
   an, bn = map(int, input().split())
   a = list(map(int, input().split()))
   b = list(map(int, input().split()))
-
-  a.sort(reverse=True)
-  b.sort(reverse=True)
-
+  a.sort()
+  b.sort()
   count = 0
-  for j in range(an):
-    maxCount = 0
-    for k in range(bn):
-      if a[j] > b[k]:
-        count = bn - maxCount + count
-        break
-      else:
-        maxCount += 1
 
-  result.append(count)
+  for j in a:
+    count += binary_search(j, b) + 1
 
-for n in range(t):
-  print(result[n])
+  print(count)
