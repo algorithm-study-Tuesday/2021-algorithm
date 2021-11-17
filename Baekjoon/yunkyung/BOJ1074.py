29a96ebcd8370fcb27975a263cea642b78df6885
expresshihway <03wow22@gmail.com>
2021-11-17 10:27:24 +0900

    feat: BOJ1074 Z

---
+++ Baekjoon/yunkyung/BOJ1074.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N, r, c = map(int, input().split())
+num = 0
+
+while N > 0:
+  temp = (2**N) // 2
+  if N > 1:
+    if r < temp and c >= temp:
+      num += temp**2
+      c -= temp
+    elif r >= temp and c < temp:
+      num += (temp**2) * 2
+      r -= temp
+    elif r >= temp and c >= temp:
+      num += (temp**2) * 3
+      r -= temp
+      c -= temp
+  elif N == 1:
+    if r == 0 and c == 1:
+      num += 1
+    elif r == 1 and c == 0:
+      num += 2
+    elif r == 1 and c == 1:
+      num += 3
+  N -= 1
+print(num)
