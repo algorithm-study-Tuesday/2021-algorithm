7bd7a604f99c21cd8871d6a7145f30f16039da57
hyunJIN7 <35680342+hyunJIN7@users.noreply.github.com>
2021-12-15 15:19:47 +0900

    Add files via upload

---
+++ Baekjoon/hyunjin/BOJ11279.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int n;
+	cin >> n;
+	priority_queue<ll> pq;
+	while(n--){
+		ll calc; 
+		cin>>calc;
+		if(calc){
+			pq.push(calc);
+		}
+		else{
+			if(!pq.size()) cout<<"0\n";
+			else {
+				cout<<pq.top()<<"\n";
+				pq.pop();
+					
+			}
+		}
+	}
+}
